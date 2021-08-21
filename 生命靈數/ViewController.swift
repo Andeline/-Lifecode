90cdc388e139abf516f51d002d29d3c77fcf0de1
Andeline <andelineya@gmail.com>
2021-08-21 23:32:07 +0800

    Initial Commit

---
+++ 生命靈數/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  生命靈數
+//
+//  Created by 蔡佳穎 on 2021/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
